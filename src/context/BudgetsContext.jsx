8470884b4fddb5e9070fe4f28b26561a33804915
Rishab <rlamba3@uic.edu>
2022-01-15 19:49:42 -0600

    UPDATE: budgetContext hook created

--- src/context/BudgetsContext.jsx
+++ src/context/BudgetsContext.jsx
@@ -1,5 +1,56 @@
-export function useBudgets() {}
+import { createContext, useContext, useState } from 'react';
+import { nanoid } from 'nanoid';
+
+const BudgetsContext = createContext();
+
+export function useBudgets() {
+    return useContext(BudgetsContext);
+}
 
 export const BudgetsProvider = ({ children }) => {
-    return children;
+    const [budgets, setBudgets] = useState([]);
+    const [expenses, setExpenses] = useState([]);
+
+    const getBudgetExpenses = (budgetId) => {
+        return expenses.filter((expense) => expense.budgetId === budgetId);
+    };
+    const addBudget = ({ name, max }) => {
+        setBudgets((prevBudgets) => {
+            if (prevBudgets.find((budget) => budget.name === name))
+                return prevBudgets;
+            return [...prevBudgets, { id: nanoid(), name, max }];
+        });
+    };
+    const addExpense = ({ description, amount, budgetId }) => {
+        setExpenses((prevExpenses) => {
+            return [
+                ...prevExpenses,
+                { id: nanoid(), description, amount, budgetId },
+            ];
+        });
+    };
+    const deleteBudget = ({ id }) =>
+        setBudgets((prevBudgets) =>
+            prevBudgets.filter((budget) => budget.id !== id)
+        );
+    const deleteExpense = ({ id }) =>
+        setExpenses((prevExpenses) =>
+            prevExpenses.filter((expense) => expense.id !== id)
+        );
+
+    return (
+        <BudgetsContext.Provider
+            value={{
+                budgets,
+                expenses,
+                getBudgetExpenses,
+                addExpense,
+                addBudget,
+                deleteBudget,
+                deleteExpense,
+            }}
+        >
+            {children}
+        </BudgetsContext.Provider>
+    );
 };
